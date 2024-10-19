0937a7daf963af284545e2d1bea8609051a344c0
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2024-10-19 17:31:05 +0200

    Passing the app into SSG

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,10 @@ export default defineNuxtConfig({
       viewport: 'width=device-width, initial-scale=1',
     },
   },
-  ssr: false,
+  ssr: true,
+  nitro: {
+    preset: 'static',
+  },
   components: [
     {
       path: '~/components',
