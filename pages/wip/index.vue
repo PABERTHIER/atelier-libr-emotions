7f798472e1bffbc35308a7a7da79dbd30da8be53
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2024-11-01 16:17:59 +0100

    Add wip page

---
+++ pages/wip/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="wip">
+    <NuxtImg src="wip.gif" sizes="sm:400px, md:700px, lg:1000px" densities="x1 x2" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.wip {
+  display: flex;
+  justify-content: center;
+  margin-top: 10%;
+}
+</style>
