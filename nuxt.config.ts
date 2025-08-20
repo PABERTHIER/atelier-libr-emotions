ce444692111fe9ce93cf8f487ba50d83f41b5870
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2025-08-20 20:18:47 +0200

    Fix Vite config since Vite 7 is now used

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -101,7 +101,6 @@ export default defineNuxtConfig({
       preprocessorOptions: {
         scss: {
           additionalData: '@use "@/styles/variables.scss" as *;',
-          api: 'modern-compiler',
         },
       },
     },
