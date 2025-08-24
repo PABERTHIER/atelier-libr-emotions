5a13196c697cc12a9b113c588873b28939b0f996
Pierre-Antoine Berthier <39381248+PABERTHIER@users.noreply.github.com>
2025-08-24 20:54:59 +0200

    ⚗️ Menu improvements (#28)

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,17 +1,5 @@
 <template>
-  <div class="about">
-    <NuxtLink
-      :to="facebookLink"
-      aria-label="Facebook"
-      title="Facebook"
-      target="_blank">
-      <Icon
-        name="ale-icon:facebook"
-        size="25px"
-        mode="svg"
-        aria-hidden="true" />
-    </NuxtLink>
-  </div>
+  <div class="about"></div>
 </template>
 
 <script setup lang="ts">
@@ -20,15 +8,10 @@ const { t } = useI18n()
 useHead({
   title: computed(() => t('pages.about.tab_name')),
 })
-
-const facebookLink = computed(
-  () => 'https://www.facebook.com/people/Atelier-LibrEmotions/100063484446329/'
-)
 </script>
 
 <style lang="scss" scoped>
 .about {
-  height: 100%;
   background: $background;
   padding: 10px;
 }
---
+++ pages/contact/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="contact">
+    <NuxtLink
+      :to="facebookLink"
+      aria-label="Facebook"
+      title="Facebook"
+      target="_blank">
+      <Icon
+        name="ale-icon:facebook"
+        size="25px"
+        mode="svg"
+        aria-hidden="true" />
+    </NuxtLink>
+    <NuxtLink
+      :to="instagramLink"
+      aria-label="Instagram"
+      title="Instagram"
+      target="_blank">
+      <Icon
+        name="ale-icon:instagram"
+        size="25px"
+        mode="svg"
+        aria-hidden="true" />
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n()
+
+useHead({
+  title: computed(() => t('pages.contact.tab_name')),
+})
+
+const facebookLink = computed(
+  () => 'https://www.facebook.com/people/Atelier-LibrEmotions/100063484446329/'
+)
+const instagramLink = computed(
+  () => 'https://www.instagram.com/atelier_libremotions'
+)
+</script>
+
+<style lang="scss" scoped>
+.contact {
+  padding: 10px;
+}
+</style>
