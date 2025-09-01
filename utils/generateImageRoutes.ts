3aae0a8d8414aefcfdc98182106cb78b081ee2be
Pierre-Antoine Berthier <pierreantoinebrt@gmail.com>
2025-09-01 20:58:02 +0200

    Add webp images + image routes generation utils for Nitro

---
+++ utils/generateImageRoutes.ts
@@ -0,0 +1,33 @@
+export function generateImageRoutes() {
+  const routes = []
+
+  for (const image of imagesConfig) {
+    for (const size of image.sizes) {
+      routes.push(`/_ipx/w_${size}${image.imagePath}`)
+      routes.push(`/_ipx/w_${size}&dpr_2${image.imagePath}`) // For retina displays
+    }
+  }
+
+  return routes
+}
+
+const imagesConfig = [
+  {
+    imagePath: '/paintings/celestial-collision.webp',
+    sizes: [
+      80, 100, 125, 130, 150, 160, 175, 180, 200, 250, 260, 300, 350, 360, 400,
+    ],
+  },
+  {
+    imagePath: '/paintings/entangled-echoes.webp',
+    sizes: [
+      80, 100, 125, 130, 150, 160, 175, 180, 200, 250, 260, 300, 350, 360, 400,
+    ],
+  },
+  {
+    imagePath: '/paintings/abstract-autumn.webp',
+    sizes: [
+      80, 100, 125, 130, 150, 160, 175, 180, 200, 250, 260, 300, 350, 360, 400,
+    ],
+  },
+]
