023d9b56f2ef892868144b30a8e2e30bf97d7d5e
Pierre-Antoine Berthier <39381248+PABERTHIER@users.noreply.github.com>
2025-09-13 16:52:13 +0200

    🎨 Improve Menu, Homepage and Contact pages (#41)

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -7,12 +7,10 @@ const { t } = useI18n()
 
 useHead({
   title: computed(() => t('pages.about.tab_name')),
+  meta: [
+    { name: 'description', content: computed(() => t('app.meta.description')) },
+  ],
 })
 </script>
 
-<style lang="scss" scoped>
-.about {
-  background: $background;
-  padding: 10px;
-}
-</style>
+<style lang="scss" scoped></style>
