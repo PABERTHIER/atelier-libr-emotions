48e0089b8d078b502c4df52de0f459caddbf4074
Pierre-Antoine Berthier <39381248+PABERTHIER@users.noreply.github.com>
2026-02-28 14:57:45 +0100

    🔥 Fix image generation on mobile since Vercel update (#73)

--- app/utils/generateImageRoutes.ts
+++ app/utils/generateImageRoutes.ts
@@ -6,6 +6,8 @@ export function generateImageRoutes() {
       routes.push(`/_ipx/w_${size}${image.imagePath}`)
       routes.push(`/_ipx/w_${size}&dpr_2${image.imagePath}`) // For retina displays
     }
+
+    routes.push(`/_ipx/q_50&blur_3&s_10x10/${image.imagePath}`)
   }
 
   return routes
@@ -15,8 +17,8 @@ const imagesConfig = [
   {
     imagePath: '/paintings/celestial-collision.webp',
     sizes: [
-      20, 30, 35, 40, 80, 100, 125, 130, 150, 160, 175, 180, 200, 250, 260, 300,
-      350, 360, 400,
+      20, 30, 35, 40, 60, 70, 80, 100, 125, 130, 150, 160, 175, 180, 200, 250,
+      260, 300, 350, 360, 400,
     ],
   },
   {
@@ -29,22 +31,23 @@ const imagesConfig = [
     imagePath: '/paintings/abstract-autumn.webp',
     sizes: [
       80, 100, 125, 130, 150, 160, 175, 180, 200, 250, 260, 300, 350, 360, 400,
+      500, 700, 1000,
     ],
   },
   {
     imagePath: '/paintings/some-paintings.webp',
-    sizes: [150, 200, 250, 300, 400],
+    sizes: [150, 200, 250, 300, 400, 500, 600, 800],
   },
   {
     imagePath: '/cv/page_1.webp',
-    sizes: [600, 900, 1200],
+    sizes: [300, 450, 600, 900, 1200],
   },
   {
     imagePath: '/cv/page_2.webp',
-    sizes: [600, 900, 1200],
+    sizes: [300, 450, 600, 900, 1200],
   },
   {
     imagePath: '/wip.gif',
-    sizes: [400, 700, 1000],
+    sizes: [400, 700, 800, 1000, 1400, 2000],
   },
 ]
